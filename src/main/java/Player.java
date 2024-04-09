ae6f12a16ddc4d75274c9ee1819ea88ef2d98849
Tatyana Gerasina <gtstata@gmail.com>
2024-04-09 14:13:03 +0300

    Created Game and Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,39 @@
+import java.util.Objects;
+
 public class Player {
+    protected int id;
+    protected String name;
+    protected int strength;
+
+    public Player(int id, String name, int strength) {
+        this.id = id;
+        this.name = name;
+        this.strength = strength;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return id == player.id && strength == player.strength && name.equals(player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, strength);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getStrength() {
+
+        return strength;
+    }
 }
+
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,79 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GameTest {
+
+    @Test
+    public void shouldWhenFirstPlayerWin() {
+        Player Ivan = new Player(1,"Ivan",100);
+        Player Fedor = new Player(2,"Fedor",40);
+
+        Game game = new Game();
+
+        game.register(Ivan);
+        game.register(Fedor);
+
+        int actual = game.round("Ivan","Fedor");
+        int expected = 1;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldWhenSecondPlayerWin() {
+        Player Ivan = new Player(1,"Ivan",100);
+        Player Fedor = new Player(2,"Fedor",140);
+
+        Game game = new Game();
+
+        game.register(Ivan);
+        game.register(Fedor);
+
+        int actual = game.round("Ivan","Fedor");
+        int expected = 2;
+
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldWhenPlayerEqualStrenghts() {
+        Player Ivan = new Player(1,"Ivan",100);
+        Player Fedor = new Player(2,"Fedor",100);
+
+        Game game = new Game();
+
+        game.register(Ivan);
+        game.register(Fedor);
+
+        int actual = game.round("Ivan","Fedor");
+        int expected = 0;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldWhenPlayer1NotRegistered() {
+        Player Fedor = new Player(2,"Fedor",100);
+
+        Game game = new Game();
+
+        game.register(Fedor);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                ()-> game.round("Mary","Fedor")
+        );
+    }
+
+    @Test
+    public void shouldWhenPlayer2NotRegistered() {
+        Player Ivan = new Player(1,"Ivan",100);
+
+        Game game = new Game();
+
+        game.register(Ivan);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                ()-> game.round("Ivan","Ann")
+        );
+    }
+
 }
